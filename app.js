6c287e9331493a9bf4fbb6fbda65f67fdc560326
Takon Ajie Njar <44798140+nkimajie@users.noreply.github.com>
2022-10-26 00:25:58 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const port = 3033;
 app.get('/online', function(req, res) {
-    res.json({ message: "Success! App is online" })
+    res.json({ message: "Success! App is online omohhh" })
 })
 
 
